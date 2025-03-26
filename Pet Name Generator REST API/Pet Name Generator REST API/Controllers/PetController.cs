b2d663bc032933bbe58010be9af41129322c2d64
MarcChristianDelosSa <MarcChristianDelosSa@SMU-0120>
2025-03-26 16:13:17 +0800

    Pet Name Generator REST API

---
+++ Pet Name Generator REST API/Pet Name Generator REST API/Controllers/PetController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+[ApiController]
+[Route("api/petname")]
+public class PetNameController : ControllerBase
+{
+    private static readonly string[] DogNames = { "Renato", "Charles", "Lance", "Felix", "Rohann" };
+    private static readonly string[] CatNames = { "Poosee", "Whitey", "Blackie", "Powsi", "Pspsps" };
+    private static readonly string[] BirdNames = { "Twitter", "Birdie", "Cheerp", "Raven", "Maya" };
+
+    [HttpPost("generate")]
+    public IActionResult Generate([FromBody] PetNameRequest request)
+    {
+        string[] names;
+
+        switch (request.AnimalType.ToLower())
+        {
+            case "dog":
+                names = DogNames;
+                break;
+            case "cat":
+                names = CatNames;
+                break;
+            case "bird":
+                names = BirdNames;
+                break;
+            default:
+                return BadRequest(new { error = "Invalid animal type." });
+        }
+
+        var random = new Random();
+        string petName = names[random.Next(names.Length)] +
+            (request.TwoPart == true ? " " + names[random.Next(names.Length)] : "");
+
+        return Ok(new { name = petName });
+    }
+}
+
+public class PetNameRequest
+{
+    public string AnimalType { get; set; }
+    public bool? TwoPart { get; set; }
+}
